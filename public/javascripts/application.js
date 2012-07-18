4058d850d3e9276cc5de7c3001112a8f25f19b07
dzucconi <damonzucconi@gmail.com>
2012-07-18 12:22:29 -0400

    par

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,36 +3,44 @@ $(function() {
   $(document).on('click', '.connections a', function(e) {
     e.preventDefault();
 
-    $('#loading').show()
-    $(this).addClass('loading');
+    Utils.startLoad(this);
 
     $.ajax({
       url: $(this).attr('href'),
       success: function(response) {
-        $('#loading').hide();
-        $('.connections a').removeClass('loading')
-        $('#main').append(response);
-        
-        var _peek = 250;
-        var _offset = $('#main').width() - $('.column-wrap').last().width() - _peek;
-        $('body').animate({scrollLeft: _offset}, 'fast');
+        Utils.stopLoad();
+
+        $(response)
+          .hide()
+          .appendTo('#main')
+          .fadeIn(250, function() {
+            var offset = 0;
+
+            $('.column-wrap').each(function(index) {
+              offset += $(this).width();
+            });
+
+            $('#main').animate({scrollLeft: offset}, 250);
+
+          });
       }
     });
   });
 
-  // Reset
+  // Scroll back
   $(document).on('click', '#fp', function() {
-    $('body').animate({scrollLeft: 0}, 'fast');
+    $('#main').animate({scrollLeft: 0}, 'fast');
   })
 });
 
-// Bullshit
 var Utils = {
-  lazy: function() {
-    $('img').lazyload({
-      container: $(".stream"),
-      effect : "fadeIn",
-      threshold : 200
-    });
+  startLoad: function(_this) {
+    $('#loading').show()
+    $(_this).addClass('loading');
+  },
+
+  stopLoad: function() {
+    $('#loading').hide();
+    $('.connections a').removeClass('loading');
   }
 }
